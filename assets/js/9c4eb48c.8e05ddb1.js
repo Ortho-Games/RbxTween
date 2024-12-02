a34f9016cb96966af6a50c131c09c36533f23d19
DylWithlt <driscoll_dylan@yahoo.com>
2024-12-01 19:11:58 -0500

    Built and published by Moonwave

---
+++ assets/js/9c4eb48c.8e05ddb1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[431],{7676:e=>{e.exports=JSON.parse('[{"type":"link","href":"/api/RbxTween","label":"\u200bRbx\u200bTween"},{"type":"link","href":"/api/Timeline","label":"\u200bTimeline"},{"type":"link","href":"/api/Tween","label":"\u200bTween"}]')}}]);
--- src/Types.lua
+++
@@ -1,4 +0,0 @@
-export type EasingFunction = (t: number) -> number
-export type DirectionFunction = (ease: EasingFunction) -> (t: number) -> number
-
-return {}
